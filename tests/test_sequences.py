2377e14b5d100c8c387c0343e3ae112c796cac97
Ruan Comelli <ruancomelli@gmail.com>
2021-04-26 20:48:45 -0300

    Achieve 100% coverage in testing sequences module

--- tests/test_sequences.py
+++ tests/test_sequences.py
@@ -10,3 +10,12 @@ def test_MutableSequence():
     assert seq[2] == 15
     assert 3 in seq
     assert 0 not in seq
+    assert str(seq) == 'IntSeq([3, 14, 15])'
+    assert repr(seq) == 'IntSeq([3, 14, 15])'
+
+    assert seq[1] == 14
+    seq[1] = -14
+    assert seq[1] == -14
+
+    del seq[1]
+    assert len(seq) == 2
--- wordmaze/utils/sequences.py
+++ wordmaze/utils/sequences.py
@@ -12,6 +12,9 @@ class MutableSequence(BaseSequence[_Entry]):
     def __str__(self) -> str:
         return f'{self.__class__.__name__}({self.__entries})'
 
+    def __repr__(self) -> str:
+        return str(self)
+
     def __getitem__(self, index: int) -> _Entry:
         return self.__entries[index]
 
